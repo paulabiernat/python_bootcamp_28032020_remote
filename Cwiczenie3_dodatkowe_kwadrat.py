e6e75991399f822859e1014708dbd84955ae6775
PAULA <paula@MacBook-Air-PAULA.local>
2020-04-15 21:34:51 +0200

    zadanka

--- Cwiczenie3_dodatkowe_kwadrat.py
+++ Cwiczenie3_dodatkowe_kwadrat.py
@@ -1,10 +1,7 @@
 
 x = int(input("podaj liczbe: "))
 
-z =int(x/x)
-
 print(x * "*")
 for i in range (2,x):
-    print(f'{z * "*"}{" " * (x-2)}{z * "*"}')
+    print(f'*{" " * (x-2)}*')
 print(x * "*")
-
--- cwiczenie1_dodatkowe_tekst.py
+++ cwiczenie1_dodatkowe_tekst.py
@@ -9,6 +9,9 @@ space2 = " ".join(y)
 print(space1)
 print(space2)
 
+
+#HELLO STRANGER!
+
 print(f" {napis[0]} {napis[2]} {napis[4]} {napis[6]} {napis[8]} {napis[10]} {napis[12]} {napis[14]}")
 print(f" {napis[1]} {napis[3]} {napis[7]} {napis[9]} {napis[11]} {napis[13]}")
 
