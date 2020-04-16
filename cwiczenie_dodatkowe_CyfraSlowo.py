69149f09d67f59fdaa11c254a8576b42c4a3c936
PAULA <paula@MacBook-Air-PAULA.local>
2020-04-16 16:24:52 +0200

    opis

--- cwiczenie_dodatkowe_CyfraSlowo.py
+++
@@ -1,7 +0,0 @@
-x = input("Podaj liczbe: ")
-
-for y in x:
-   cyfra =('0','1','2','3','4','5','6','7','8','9')
-   if y in cyfra:
-       slowo=("zero","jeden","dwa","trzy","cztery","pięć","sześć","siedem","osiem","dziewięć")
-       print(slowo[int(y)])
