1b59b98ae9f39310db9d3f085bd3b80e9413d75e
PAULA <paula@MacBook-Air-PAULA.local>
2020-04-17 20:02:12 +0200

    zadanie

--- CWCZENIE2_dodatkowe_podwojenie.py
+++ CWCZENIE2_dodatkowe_podwojenie.py
@@ -1,3 +1,14 @@
+# Wersja 1
+
+
 tekst = input("Podaj tekst: ")
 
 print(f"{tekst[0:1]}{tekst[1]*2}{tekst[2:3]}{tekst[3]*2}{tekst[4:5]}{tekst[5]*2}{tekst[6:7]}{tekst[7]*2}{tekst[8:9]}{tekst[9]*2}")
+
+
+# Wersja 2.
+x = input("podaj tekst: ")
+y = len(x)
+
+if i in range (y):
+    if i % 2 == 0:
---
+++ doda.py
@@ -0,0 +1,15 @@
+
+
+x = input('podaj napis: ')
+y = len(x)
+print(float(y))
+
+for i in y:
+    pr
+
+lista = []
+while len(lista) < 10:
+    m = int(input('wprowadz liczbe : '))
+    lista.append(m)
+print(sum(lista)/len(lista))
+
